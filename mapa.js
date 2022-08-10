9713530660f7f2876c86cbc0c65c582e4c391ddd
feliann <101577329+feliann@users.noreply.github.com>
2022-08-10 14:42:40 -0300

    map

--- mapa.js
+++ mapa.js
@@ -1,7 +1,7 @@
 let map = L.map('map').setView([-34.612918138033976, -58.43811578328029],12.3)
  
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+L.tileLayer('https://maps.googleapis.com/maps/vt?pb=!1m5!1m4!1i{z}!2i{x}!3i{y}!4i256!2m3!1e0!2sm!3i349018013!3m9!2sen-US!3sUS!5e18!12m1!1e47!12m3!1e37!2m1!1ssmartmaps!4e0',{
+            attribution: '&copy; Powered by Google'
 }).addTo(map);
  
 document.getElementById('select-location').addEventListener('change', function(e){
